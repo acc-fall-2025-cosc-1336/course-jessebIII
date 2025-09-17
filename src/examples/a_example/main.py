aaa149a7640756c3e931d43fa3e529b06d35dbfb
jessebIII <jesse.borunda@g.austincc.edu>
2025-09-17 01:27:45 +0000

    imported function from devprocess into main

--- src/examples/a_example/main.py
+++ src/examples/a_example/main.py
@@ -1,7 +1,4 @@
 from devprocess import greet_software_developer
 
-def main():
-    # Call the imported function
-
-    if  __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    greet_software_developer()
