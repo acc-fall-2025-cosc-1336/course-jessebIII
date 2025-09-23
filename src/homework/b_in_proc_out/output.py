2673cd441c07004e5bea0c6c859666e39c07b406
jessebIII <jesse.borunda@g.austincc.edu>
2025-09-23 02:17:25 +0000

    Tests

--- src/homework/b_in_proc_out/output.py
+++ src/homework/b_in_proc_out/output.py
@@ -1,6 +1,10 @@
 # named constant
 TAX_RATE = 0.0675  # 6.75%
 
+def get_number(num: int) -> int:
+    """Return the number passed in (for testing purposes)."""
+    return num
+
 def get_sales_tax_amount(meal_amount: float) -> float:
     """Calculate the sales tax amount on a meal."""
     return meal_amount * TAX_RATE
