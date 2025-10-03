1549e0f7da5911e18c20b127656d95d75e70679b
jessebIII <jesse.borunda@g.austincc.edu>
2025-10-03 21:55:45 +0000

    Test written.

--- tests/homework/c_decisions/tests_decisions.py
+++ tests/homework/c_decisions/tests_decisions.py
@@ -1,1 +1,31 @@
 import unittest
+from src.homework
+
+class TestLetterGrade(unittest.TestCase):
+    def Test_A_grade(self):
+        self.assertEqual(get_letter_grade(95), 'A')
+        self.assertEqual(get_letter_grade(90), 'A')
+        self.assertEqual(get_letter_grade(100), 'A')
+
+    def Test_B_grade(self):
+        self.assertEqual(get_letter_grade(85), 'B')
+        self.assertEqual(get_letter_grade(80), 'B')
+        self.assertEqual(get_letter_grade(89), 'B')
+    
+    def Test_C_grade(self):
+        self.assertEqual(get_letter_grade(75), 'C')
+        self.assertEqual(get_letter_grade(70), 'C')
+        self.assertEqual(get_letter_grade(79), 'C')
+
+    def Test_D_grade(self):
+        self.assertEqual(get_letter_grade(65), 'D')
+        self.assertEqual(get_letter_grade(60), 'D')
+        self.assertEqual(get_letter_grade(69), 'D')
+
+    def Test_F_grade(self):
+        self.assertEqual(get_letter_grade(50), 'F')
+        self.assertEqual(get_letter_grade(0), 'F')
+        self.assertEqual(get_letter_grade(59), 'F')
+
+if __name__ == '__main__':
+    unittest.main()
