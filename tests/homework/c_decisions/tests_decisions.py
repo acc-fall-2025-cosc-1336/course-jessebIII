4fae6a70d323c25ac1cba5a28fa461cc97fa5fde
jessebIII <jesse.borunda@g.austincc.edu>
2025-10-04 19:20:24 +0000

    Main menu input

--- tests/homework/c_decisions/tests_decisions.py
+++ tests/homework/c_decisions/tests_decisions.py
@@ -5,30 +5,13 @@ import unittest
 from homework.c_decisions.decisions import get_letter_grade
 
 class TestLetterGrade(unittest.TestCase):
-    def Test_A_grade(self):
+
+    def test_table3_cases(self):
         self.assertEqual(get_letter_grade(95), 'A')
-        self.assertEqual(get_letter_grade(90), 'A')
-        self.assertEqual(get_letter_grade(100), 'A')
-
-    def Test_B_grade(self):
         self.assertEqual(get_letter_grade(85), 'B')
-        self.assertEqual(get_letter_grade(80), 'B')
-        self.assertEqual(get_letter_grade(89), 'B')
-    
-    def Test_C_grade(self):
         self.assertEqual(get_letter_grade(75), 'C')
-        self.assertEqual(get_letter_grade(70), 'C')
-        self.assertEqual(get_letter_grade(79), 'C')
-
-    def Test_D_grade(self):
         self.assertEqual(get_letter_grade(65), 'D')
-        self.assertEqual(get_letter_grade(60), 'D')
-        self.assertEqual(get_letter_grade(69), 'D')
-
-    def Test_F_grade(self):
         self.assertEqual(get_letter_grade(50), 'F')
-        self.assertEqual(get_letter_grade(0), 'F')
-        self.assertEqual(get_letter_grade(59), 'F')
 
 if __name__ == '__main__':
     unittest.main()
