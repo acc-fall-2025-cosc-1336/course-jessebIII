7a2abcf1e36a473b6257deba511327d40da62bda
jessebIII <jesse.borunda@g.austincc.edu>
2025-11-08 23:47:04 +0000

    Hamming distance

--- src/homework/h_strings/strings.py
+++ src/homework/h_strings/strings.py
@@ -1,1 +1,14 @@
-#
+# use loops only; avoid string slicing
+def get_hamming_distance(dna1, dna2):
+    if len(dna1) != len(dna2):
+        raise ValueError("Sequences must be of equal length")
+
+    distance = 0
+    i = 0
+    while i < len(dna1):
+        a = dna1[i].upper()
+        b = dna2[i].upper()
+        if a != b:
+            distance += 1
+        i += 1
+    return distance
