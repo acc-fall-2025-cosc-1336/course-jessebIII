1050c8441cfe4dca5b1c1d90bd4026ae3f4689da
jessebIII <jesse.borunda@g.austincc.edu>
2025-11-09 17:46:23 +0000

    highest list value with loops

--- src/homework/g_lists_and_tuples/lists.py
+++ src/homework/g_lists_and_tuples/lists.py
@@ -19,3 +19,21 @@ def get_lowest_list_value(values):
 		i += 1
 
 	return lowest
+
+
+	def get_highest_list_value(values):
+		"""Return the highest value in the list `values` using a loop.
+
+		Raises ValueError if the list is empty.
+		"""
+		if values is None or len(values) == 0:
+			raise ValueError("values must be a non-empty list")
+
+		highest = values[0]
+		i = 1
+		while i < len(values):
+			if values[i] > highest:
+				highest = values[i]
+			i += 1
+
+		return highest
