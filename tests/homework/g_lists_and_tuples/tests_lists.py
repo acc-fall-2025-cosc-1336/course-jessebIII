bf3ac33a246599027d9687b2859018bcf356f784
jessebIII <jesse.borunda@g.austincc.edu>
2025-11-09 17:47:07 +0000

    unit test 1

---
+++ tests/homework/g_lists_and_tuples/tests_lists.py
@@ -0,0 +1,17 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
+
+import unittest
+from src.homework.g_lists_and_tuples.lists import get_lowest_list_value
+
+
+class Test_Config(unittest.TestCase):
+
+    def test_get_lowest_list_value(self):
+        values = [8, 10, 1, 50, 20]
+        self.assertEqual(get_lowest_list_value(values), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
