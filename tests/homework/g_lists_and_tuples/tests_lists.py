f6b2323dc14039ed4dc2600c43760f4bff0c0b2c
jessebIII <jesse.borunda@g.austincc.edu>
2025-11-09 17:47:53 +0000

    unit test 2

--- tests/homework/g_lists_and_tuples/tests_lists.py
+++ tests/homework/g_lists_and_tuples/tests_lists.py
@@ -3,7 +3,7 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
 
 import unittest
-from src.homework.g_lists_and_tuples.lists import get_lowest_list_value
+from src.homework.g_lists_and_tuples.lists import get_lowest_list_value, get_highest_list_value
 
 
 class Test_Config(unittest.TestCase):
@@ -11,6 +11,10 @@ class Test_Config(unittest.TestCase):
     def test_get_lowest_list_value(self):
         values = [8, 10, 1, 50, 20]
         self.assertEqual(get_lowest_list_value(values), 1)
+
+    def test_get_highest_list_value(self):
+        values = [8, 10, 1, 50, 20]
+        self.assertEqual(get_highest_list_value(values), 50)
 
 
 if __name__ == '__main__':
