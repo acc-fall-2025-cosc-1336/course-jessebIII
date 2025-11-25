14b29637c01493397fcf1e2a30529f00e8744dc8
jessebIII <jesse.borunda@g.austincc.edu>
2025-11-25 05:24:25 +0000

    unit test revised. Fixing SRC error

--- tests/homework/i_dictionaries_sets/test_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/test_dictionaries_and_sets.py
@@ -1,3 +1,9 @@
+import os
+import sys
 import unittest
-from src.homework.i_dictionaries_and_sets import get_p_distance
-from src.homework.i_dictionaries_and_sets import get_p_distance
+
+# allow importing `src` when running this test file directly
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..')))
+
+from src.homework.i_dictionaries_sets import get_p_distance
+from src.homework.i_dictionaries_sets import get_p_distance
